01031c33fb88869c5d26032abc2ce9270efa6da0
Mark <mark.christiansen@nuim.ie>
2017-04-21 22:53:02 +0100

    Add test to ensure the handling of 4th downs is done correctly

---
+++ tests/test_summariseGame.py
@@ -0,0 +1,24 @@
+from expectedPoints.summariseGame import handle_fourth_down
+
+
+def test_handle_fourth_down():
+    sample_string = [["1st-10-ROS31PassDarrel Cope pass under pressure from Joseba Aguirre. Incomplete.",
+                      "2nd-10-ROS31PassDarrel Cope pass incomplete to Sean Fawl.",
+                      "3rd-10-ROS31PassDarrel Cope pass to Francis Nugent for 7 yards."
+                      "PuntDorival Canova punts the ball for 42 yards. "
+                      "Joseph Dowds returns for 8 yards and is tackled by Jamsie McDaid."
+                      "annic Steudler returns for 1 yards and is tackled by Saša Mirosavljev."],
+                     ["1st-10-TRA34PassHunter Staten pass to Joseph Moss for 22 yards for a first down!",
+                      "1st-10-TRA56PassHunter Staten pass incomplete to Torsten Dreier.",
+                      "2nd-10-TRA56PassHunter Staten pass to Joseph Moss for 23 yards for a first down!",
+                      "1st-10-TRA79PassHunter Staten pass incomplete to Joseph Moss.",
+                      "2nd-10-TRA79PassHunter Staten pass incomplete to Rodolfo Belmonte.",
+                      "3rd-10-TRA79PassHunter Staten pass incomplete to Joseph Moss."
+                      "Field goalJoseph Moss 38 yard field goal is GOOD."
+                      "KickoffJoseph Moss performs the kickoff. He kicks the ball for 65 yards. "
+                      "Sean Fawl returns for 26 yards and is tackled by Gavin Hartery. "
+                      "The offense will start on 69 yards from the end zone."]]
+    answer = handle_fourth_down(sample_string)
+    assert(answer[0][3][4:6] == "03")
+    assert(answer[1][6][10:12] == "79")
+    assert("field goal is GOOD" in answer[1][6])
