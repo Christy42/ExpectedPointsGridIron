451df58023aeb1cd174ecfb246be4ab0bbe070d6
Mark <mark.christiansen@nuim.ie>
2017-04-22 23:20:01 +0100

    Update filter files to the style of the dictionaries

--- expectedPoints/filter_files.py
+++ expectedPoints/filter_files.py
@@ -5,12 +5,18 @@ from expectedPoints.utility import add_dicts
 
 
 def cycle_files(folder):
-    values = {}
-    number_of = {}
+    values = {"off": {}, "def": {}}
+    number_of = {"off": {}, "def": {}}
     for filename in os.listdir(folder):
-        with open(filename, "w") as file:
+        with open(folder + "//" + filename, "r") as file:
             game_text = file.read()
         value_game = summariseGame.summarise_game(game_text)
-        values = add_dicts(values, value_game["values"])
-        number_of = add_dicts(number_of, value_game["number_of"])
+        values["off"] = add_dicts(values["off"], value_game["values"]["off"])
+        values["def"] = add_dicts(values["def"], value_game["values"]["def"])
+        number_of["off"] = add_dicts(number_of["off"], value_game["number_of"]["off"])
+        number_of["def"] = add_dicts(number_of["def"], value_game["number_of"]["def"])
     return {"values": values, "number_of": number_of}
+
+beta = cycle_files("C://Users//Mark//PycharmProjects//pointsEstimationFolder//BaseAttempt")
+print(beta["values"]["off"])
+print(beta["number_of"]["off"])
