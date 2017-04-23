99cb7d2ebee885c1843f6e1b0e6e7d94c93df32b
Mark <mark.christiansen@nuim.ie>
2017-04-23 12:42:18 +0100

    Let filter files output results to a csv file

--- expectedPoints/filter_files.py
+++ expectedPoints/filter_files.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 from expectedPoints import summariseGame
 from expectedPoints.utility import add_dicts
@@ -15,8 +16,18 @@ def cycle_files(folder):
         values["def"] = add_dicts(values["def"], value_game["values"]["def"])
         number_of["off"] = add_dicts(number_of["off"], value_game["number_of"]["off"])
         number_of["def"] = add_dicts(number_of["def"], value_game["number_of"]["def"])
+        with open(folder + '//values.csv', 'w') as csv_file:
+            writer = csv.writer(csv_file)
+            for key, value in values["off"].items():
+                writer.writerow([key, value])
+        with open(folder + '//number_of.csv', 'w') as csv_file:
+            writer = csv.writer(csv_file)
+            for key, value in number_of["off"].items():
+                writer.writerow([key, value])
     return {"values": values, "number_of": number_of}
 
 beta = cycle_files("C://Users//Mark//PycharmProjects//pointsEstimationFolder//BaseAttempt")
 print(beta["values"]["off"])
 print(beta["number_of"]["off"])
+# Last round done, ASE season 23 Top division
+# Round 5 - 05.11.2016 12:00Bigfoot28 - 42Brixton MassiveWeasels14 - 28Newbury NighthawksMelittlemenonthepitchHitchin119 - 0MammothSeattle Seahawks48 - 56Salamanca Road TrainsDragones de Cuera0 - 70Palafolls Almogavers
